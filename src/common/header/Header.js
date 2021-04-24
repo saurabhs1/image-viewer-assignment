22c24fb89d8c18b2c4b94af163ec4478fb5196a1
saurabhs1 <sansoss9@gmail.com>
2021-04-25 02:04:07 +0530

    Feat - Part1: Added Header

---
+++ src/common/header/Header.js
@@ -0,0 +1,24 @@
+import React, {Component, Fragment} from 'react';
+import './Header.css';  // CSS file for header
+
+class Header extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+        }
+    }
+
+    render() {
+        return <div className='header-flex-container'>
+        {
+                    <div>
+                            <header className='logo'>Image Viewer</header>
+                    </div>
+        }
+        </div>
+    }
+
+}
+
+export default Header;
